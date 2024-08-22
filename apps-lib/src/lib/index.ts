0f588399f89c0c21d8dd042f8db29d01c88ec61e
triesap <137732411+triesap@users.noreply.github.com>
2024-08-22 00:15:59 +0000

    apps-lib: initial commit

---
+++ apps-lib/src/lib/index.ts
@@ -0,0 +1,3 @@
+export * from "./locales/i18n"
+export { default as css_static } from "./ui/css_static.svelte"
+export * from "./utils/client"
---
+++ apps-lib/src/lib/locales/i18n.ts
@@ -0,0 +1,45 @@
+import i18n from '@sveltekit-i18n/base';
+import type { Config } from '@sveltekit-i18n/parser-icu';
+import parser from '@sveltekit-i18n/parser-icu';
+import locales_keys from './locales.json';
+
+type Locale = keyof typeof locales_keys;
+type LanguageConfig = {
+	default?: string;
+	value?: string;
+};
+const locales_files = [`app`] as const;
+const translations_keys: Record<Locale, any> = {
+	en: { locales_keys },
+};
+export const default_locale: Locale = `en`;
+
+const config: Config<LanguageConfig> = {
+	initLocale: default_locale,
+	parser: parser(),
+	translations: translations_keys,
+	loaders: [
+		...Object.keys(translations_keys).map((locale) => locales_files.map(key => ({
+			locale,
+			key,
+			loader: async () => (await import(`./${locale}/${key}.json`)).default
+		}))),
+	].flat()
+};
+
+const {
+	t,
+	loading: translations_loading,
+	locales,
+	locale,
+	translations,
+	loadTranslations: load_translations
+} = new i18n(config);
+
+translations_loading.subscribe(async ($loading) => {
+	if ($loading) await translations_loading.toPromise();
+});
+
+export {
+	load_translations, locale, locales, t, translations, translations_loading
+};
