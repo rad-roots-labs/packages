305c3ad190b98cdce9e8784bf2017e8b112185c1
triesap <137732411+triesap@users.noreply.github.com>
2024-09-13 07:17:45 +0000

    utils: add currency utils

--- utils/src/currency.ts
+++ utils/src/currency.ts
@@ -1,2 +1,37 @@
 export type FiatCurrency = `usd` | `eur`;
 export const fiat_currencies: FiatCurrency[] = [`usd`, `eur`] as const;
+
+export type FiatCurrencyGlyphs = `dollar` | `eur`;
+
+export function parse_currency(val?: string): FiatCurrency {
+    switch (val) {
+        case "usd":
+        case "eur":
+            return val;
+        default:
+            return `usd`;
+    };
+};
+
+export function parse_currency_glyph_key(val?: string): | `currency-${FiatCurrencyGlyphs}` {
+    switch (val) {
+        case "usd":
+            return `currency-dollar`;
+        case "eur":
+            return `currency-eur`;
+        default:
+            return `currency-dollar`;
+    };
+};
+
+export const fmt_currency_tuple = (locale: string, currency: string, amount: number): [string, string, string] => {
+    const fmt = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: parse_currency(currency).toUpperCase(),
+        minimumFractionDigits: 2,
+    });
+    const fmt_amt = fmt.format(amount);
+    const [a, b] = fmt_amt.split('.');
+    return [a.charAt(0), a.slice(1), b.length > 1 ? b : '00'];
+};
+
