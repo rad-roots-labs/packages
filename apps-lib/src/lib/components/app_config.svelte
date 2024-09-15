1a7dba30fb0ae3d55c4b2edb3982920182aa59c3
triesap <137732411+triesap@users.noreply.github.com>
2024-09-15 12:48:56 +0000

    apps-lib: add app config component

---
+++ apps-lib/src/lib/components/app_config.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { app_config, app_layout, app_thc, app_win } from "$lib";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        try {
+            app_win.set([window.innerHeight, window.innerWidth]);
+
+            const prefers_dark = window.matchMedia(
+                `(prefers-color-scheme: dark)`,
+            ).matches;
+
+            if (prefers_dark) app_thc.set(`dark`);
+            app_config.set(true);
+        } catch (e) {
+            console.log(`(layout mount) `, e);
+        } finally {
+        }
+    });
+
+    app_win.subscribe(([win_h, win_w]) => {
+        if (win_h > 800) app_layout.set("lg");
+    });
+</script>
