779ff2d8a31db1c2602470bea4664e7cad4756d2
triesap <137732411+triesap@users.noreply.github.com>
2024-09-16 13:09:33 +0000

    themes: add utils exports

--- themes/src/index.ts
+++ themes/src/index.ts
@@ -1,4 +1,4 @@
 export { theme_colors } from "./colors";
 export { themes } from "./theme";
 export type { ColorMode, Theme, ThemeKey, ThemeLayer } from "./types";
-
+export { parse_theme_key, parse_color_mode } from "./utils"
