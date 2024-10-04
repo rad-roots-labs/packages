c5b7b553f7175ef6b08cd893b5865f65c6d36a8c
triesap <137732411+triesap@users.noreply.github.com>
2024-10-04 08:14:51 +0000

    client: edit iclientnostrlib

--- client/src/nostr/lib.ts
+++ client/src/nostr/lib.ts
@@ -56,7 +56,7 @@ export class ClientNostrLib implements IClientNostrLib {
      * @returns nostr public key npub
      */
     public npub(public_key_hex: string | undefined, fallback_to_hex?: boolean): string {
-        if (!public_key_hex) return fallback_to_hex ? public_key_hex : ``;
+        if (!public_key_hex) return ``;
         const npub = nip19.npubEncode(public_key_hex);
         return npub ? npub : fallback_to_hex ? public_key_hex : ``;
     }
