5469d0357e228042e187fd3f275cdd799a9ab99c
triesap <137732411+triesap@users.noreply.github.com>
2024-10-08 10:42:00 +0000

    utils: add window conf

---
+++ utils/src/window.ts
@@ -0,0 +1,22 @@
+export type AppLayoutKey = 'mobile_base' | 'mobile_y';
+
+type ClientWindow = {
+    app: {
+        layout: Record<AppLayoutKey, {
+            h: number;
+        }>;
+    }
+};
+
+export const wind: ClientWindow = {
+    app: {
+        layout: {
+            mobile_base: {
+                h: 600
+            },
+            mobile_y: {
+                h: 768
+            }
+        }
+    }
+};
