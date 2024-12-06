27f9b50f0f69c30bdc2ed9660c6693d8d649babe
triesap <137732411+triesap@users.noreply.github.com>
2024-12-06 05:56:55 +0000

    client: remove client map

--- client/src/map/types.ts
+++
@@ -1,6 +0,0 @@
-import type { GeolocationCoordinatesPoint } from "@radroots/utils";
-
-export type IClientMap = {
-    show_map: (point: GeolocationCoordinatesPoint) => Promise<boolean>;
-    set_region: (point: GeolocationCoordinatesPoint) => Promise<boolean>;
-};
